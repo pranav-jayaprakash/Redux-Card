cde7b75f3f11ec123d3ca3f5926b274e7b2e0f28
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-09 06:44:32 +0530

    update

--- src/Components/CardFile.jsx
+++ src/Components/CardFile.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { DataFile } from '../Redux/Api'
 
 function CardFile() {
+
+    const dispatch = useDispatch()
+
+    const handleclick=(e)=>{
+        e.preventDefault()
+        
+        DataFile(dispatch)
+
+
+    }
 
 
     const take = useSelector(state=>state.Sample.Data)
     console.log(take,'XXXX')
-    
+
   return (
     <div>
         
