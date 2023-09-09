ad8a4046e46ffc02cf1ad14dec13e2c514a3d9ce
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-09 06:40:02 +0530

    update

--- src/Components/CardFile.jsx
+++ src/Components/CardFile.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function CardFile() {
+
+
+    const take = useSelector(state=>state.Sample.Data)
+    console.log(take,'XXXX')
+    
   return (
     <div>
         
