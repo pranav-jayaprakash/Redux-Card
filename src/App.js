2204e871b7b927ee01a5a2592582c68144a53fe3
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-09-09 06:47:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import CardFile from './Components/CardFile';
+
 
 function App() {
+
+  const router = createBrowserRouter([
+    {path:'/',
+  element:<CardFile/>},])
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
